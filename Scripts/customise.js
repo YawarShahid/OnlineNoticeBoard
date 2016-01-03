d7956a66d3e6bdb4b24d038c9d6fdeed3f2e5af2
BCSF12A031 <bcsf12a031@pucit.edu.pk>
2016-01-03 19:46:46 +0500

    Modify

---
+++ Scripts/customise.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.nav li').click(function (e) {
+        e.preventDefault();
+        $('.nav li').removeClass('active');
+
+        var $this = $(this);
+        if (!$this.hasClass('active')) {
+            $this.addClass('active');
+        }
+        
+    });
+});
